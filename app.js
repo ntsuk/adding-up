6f3028df6ba7b985810364e8d21c35e09d808017
ntsuk ntsuk <m.s.ntsuk@gmail.com>
2017-11-25 13:09:07 +0000

    ランキング実装式の整形（再コミット）

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const readline = require('readline');
 const rs = fs.ReadStream('./popu-pref.csv');
-const rl = readline.createInterface({ 'input' : rs, 'output' : {} });
-const map = new Map(); //key:都道府県 value: 集計データのオブジェクト
+const rl = readline.createInterface({'input': rs, 'output': {} });
+const map = new Map(); // key: 都道府県 value: 集計データのオブジェクト
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0], 10);
@@ -15,8 +15,8 @@ rl.on('line', (lineString) => {
       value = {
         popu10: 0,
         popu15: 0,
-        cange: null
-      }
+        change: null
+      };
     }
     if(year === 2010){
       value.popu10 += popu;
@@ -28,14 +28,16 @@ rl.on('line', (lineString) => {
   }
 });
 rl.resume();
-rl.on('close', () => {
+rl.on('close', () =>{
   for(let pair of map){
     const value = pair[1];
     value.change = value.popu15 / value.popu10;
   }
-  const rangingArray = Array.from(map).sort((pair1,pair2) => {return pair2[1].change - pair1[1].change})
-  const rangingStrings = rangingArray.map((pair) => {
-    return pair[0] + ': ' + pair[1].popu10 + '=>' +pair[1].popu15 + '変化率' + pair[1].change;
+  const rankingArray = Array.from(map).sort((pair1, pair2) =>{
+    return pair1[1].change - pair2[1].change;
   })
-  console.log(rangingStrings);
+  const rankingStrings = rankingArray.map((pair, i) => {
+    return (i +1) + "位 " + pair[0] + ': ' + pair[1].popu10 + '=>' + pair[1].popu15 + ' 変化率:' + pair[1].change;
+  })
+  console.log(rankingStrings);
 })
